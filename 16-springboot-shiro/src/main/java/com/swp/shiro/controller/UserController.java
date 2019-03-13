7b7ffd39ca15c28687e326f33eaea48c550afde8
shiwanpeng <15091631746@163.com>
2019-03-13 23:20:00 +0800

    111

--- 16-springboot-shiro/src/main/java/com/swp/shiro/controller/UserController.java
+++ 16-springboot-shiro/src/main/java/com/swp/shiro/controller/UserController.java
@@ -1,8 +1,19 @@
 package com.swp.shiro.controller;
 
+import com.swp.shiro.model.SysUser;
+import com.swp.shiro.repository.UserRepository;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * 描述:
@@ -14,9 +25,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository repository;
+
     @RequestMapping("/userList")
     @RequiresPermissions("userInfo:view")
-    public String userInfo(){
+    public String userInfo(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "5") int size, Model model){
+
+        Pageable pageable = PageRequest.of(page,size);
+
+        Page<SysUser> userList = repository.findAll(pageable);
+        System.out.println("users : " + userList);
+        model.addAttribute("userList",userList);
         return "userInfo";
     }
 
